d497e3bc762da6803ee5cf5107538acde2177055
Shubham Srivastava <shubham.srivastava243@gmail.com>
2021-06-12 14:04:29 +0530

    reversearrayprogram

---
+++ reversearray.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class reversearray {
+	
+
+	public static void main(String[] args)
+	{
+		int[] array = new int[] {1,2,3,4,5,6,7,8};
+		int size = array.length;
+
+		System.out.println("The reverse order of an array : ");
+		for(int i=size-1;i>=0;i--)
+		{
+			
+			System.out.println(array[i]+"");
+		}
+		
+		
+	}
+}
